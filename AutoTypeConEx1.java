853f401edc0dc740143ee07a0973585edbfa30c9
Shweta Shetty <shettyshweta48@gmail.com>
2025-01-17 19:25:22 +0530

    Add files via upload

---
+++ AutoTypeConEx1.java
@@ -0,0 +1,11 @@
+class AutoTypeConEx1 {
+    public static void main(String[] args) {
+        int i = 100;
+        long l = i;
+        float f = 1;
+        System.out.println("Int value " + i);
+        System.out.println("Long value " + l);
+        System.out.println("Float value " + f);
+
+    }
+}
